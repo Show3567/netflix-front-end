28148ddf8b2387a956038fcaa2e5f52496aa6ff5
show3567 <ydong011@fiu.edu>
2022-04-22 09:39:25 -0500

    update

--- src/app/components/register/page-four/page-four.component.ts
+++ src/app/components/register/page-four/page-four.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-four',
   templateUrl: './page-four.component.html',
-  styleUrls: ['./page-four.component.scss']
+  styleUrls: ['./page-four.component.scss'],
 })
 export class PageFourComponent implements OnInit {
+  plans = {
+    Basic: {
+      MonthlyPrice: 9.99,
+      VideoQuality: 'Good',
+      Resolution: '480p',
+    },
+    Standard: {
+      MonthlyPrice: 15.49,
+      VideoQuality: 'Better',
+      Resolution: '1080p',
+    },
+    Premium: {
+      MonthlyPrice: 19.99,
+      VideoQuality: 'Best',
+      Resolution: '4K + HDR',
+    },
+  };
+  selecedColumn = 2;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  selectPlan(num: number) {
+    this.selecedColumn = num;
   }
-
 }
