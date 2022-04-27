e3cabcd88831688f97d047cc6e92218b6bc2eeee
show3567 <ydong011@fiu.edu>
2022-04-27 01:16:08 -0500

    update

--- src/app/pages/movie-item/movie-item.component.ts
+++ src/app/pages/movie-item/movie-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { TmdbService } from 'src/app/services/tmdb.service';
 import { YouTubePlayer } from '@angular/youtube-player';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { tap, map } from 'rxjs/operators';
 
 @Component({
@@ -11,11 +11,13 @@ import { tap, map } from 'rxjs/operators';
 })
 export class MovieItemComponent implements OnInit {
   @ViewChild(YouTubePlayer, { static: true }) youTubePlayer!: YouTubePlayer;
-
+  poster_img_high: string = '';
+  backdrop_img_high: string = '';
   isMuted: boolean = false;
 
   movie: any = {};
   movieVideos: any = [];
+  companies_icons: string[] = [];
 
   size = {
     height: visualViewport.height,
@@ -24,8 +26,7 @@ export class MovieItemComponent implements OnInit {
 
   constructor(
     private tmdbService: TmdbService,
-    private activatedRoute: ActivatedRoute,
-    private router: Router
+    private activatedRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
@@ -36,12 +37,27 @@ export class MovieItemComponent implements OnInit {
           this.tmdbService.getVideo(+id).subscribe((videos: any) => {
             if (videos && videos.results) {
               this.movieVideos = [...videos.results];
-              console.log(this.movieVideos);
+              // console.log(this.movieVideos);
             }
           });
           this.tmdbService.getMovie(+id).subscribe((movie: any) => {
             this.movie = { ...movie };
             console.log(this.movie);
+
+            this.movie.production_companies.forEach((company: any) => {
+              company.logo_path !== null &&
+                this.companies_icons.push(
+                  this.tmdbService.getMovieImagePath(company.logo_path, 'w500')
+                );
+            });
+            this.backdrop_img_high = this.tmdbService.getMovieImagePath(
+              this.movie.backdrop_path,
+              'original'
+            );
+            this.poster_img_high = this.tmdbService.getMovieImagePath(
+              this.movie.poster_path,
+              'w780'
+            );
           });
         })
       )
@@ -58,5 +74,14 @@ export class MovieItemComponent implements OnInit {
     }
   }
 
-  handleError() {}
+  switchVideo(direction: string) {
+    if (direction === 'left' && this.movieVideos.length) {
+      const videoOut = this.movieVideos.shift();
+      this.movieVideos.push(videoOut);
+    } else if (direction === 'right' && this.movieVideos.length) {
+      const videoOut = this.movieVideos.pop();
+      this.movieVideos.unshift(videoOut);
+    }
+    console.log(this.movieVideos[0]);
+  }
 }
