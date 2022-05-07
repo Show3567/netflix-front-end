4ce6cb5f1655fc85a7b4f60df30dbd92f1542301
show3567 <ydong011@fiu.edu>
2022-05-07 18:51:05 -0500

    update

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 import { OverviewControlPipe } from './pipes/overview-control.pipe';
 import { MainFooterComponent } from './main/main-footer/main-footer.component';
 import { MainHeaderComponent } from './main/main-header/main-header.component';
-import { SharedModule } from '../shared/shared.module';
-import { MainQuestionsComponent } from './main/main-questions/main-questions.component';
 import { MainRowOneComponent } from './main/main-row-one/main-row-one.component';
-import { MainRowThreeComponent } from './main/main-row-three/main-row-three.component';
 import { MainRowTwoComponent } from './main/main-row-two/main-row-two.component';
+import { MainQuestionsComponent } from './main/main-questions/main-questions.component';
+import { MainRowThreeComponent } from './main/main-row-three/main-row-three.component';
 
 @NgModule({
   declarations: [
