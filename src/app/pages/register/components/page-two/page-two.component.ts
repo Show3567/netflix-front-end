15e648b001c7aa33d3395837f998935a3befbe11
show3567 <ydong011@fiu.edu>
2022-05-16 01:29:06 -0500

    update

--- src/app/pages/register/components/page-two/page-two.component.ts
+++ src/app/pages/register/components/page-two/page-two.component.ts
@@ -1,8 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, Inject, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { WithLocalstorageService } from '../../../../services/auth/with-localstorage.service';
 import { CustomValidator } from '../../../../services/validators/custom.validator';
+import { HttpClient } from '@angular/common/http';
+import { AUTHSERVER } from 'src/app/app.module';
+import { debounceTime, map, switchMap, tap, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-page-two',
@@ -11,6 +22,7 @@ import { CustomValidator } from '../../../../services/validators/custom.validato
 })
 export class PageTwoComponent implements OnInit {
   form!: FormGroup;
+  isLoading = false;
 
   get email() {
     return this.form.get('email');
@@ -22,8 +34,9 @@ export class PageTwoComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private readonly router: Router,
-    private readonly customValidator: CustomValidator,
-    private readonly authService: WithLocalstorageService
+    private readonly http: HttpClient,
+    private readonly authService: WithLocalstorageService,
+    @Inject(AUTHSERVER) private readonly authServerPath: string
   ) {}
   ngOnInit(): void {
     const initemailVal = this.authService.appNewUser.email
@@ -31,11 +44,7 @@ export class PageTwoComponent implements OnInit {
       : '';
 
     this.form = this.fb.group({
-      email: [
-        initemailVal,
-        [Validators.email],
-        [this.customValidator.hasEmail()],
-      ],
+      email: [initemailVal, [Validators.email], [this.hasEmail()]],
       password: [''],
     });
   }
@@ -44,11 +53,34 @@ export class PageTwoComponent implements OnInit {
     if (this.email?.hasError('required')) {
       return 'You must enter a value';
     }
-
     return this.email?.hasError('email') ? 'Not a valid email' : '';
   }
   onSubmit() {
     this.authService.addUserInfo(this.form.value);
     this.router.navigate(['/register/step2ii']);
+  }
+
+  /* customValidator */
+  hasEmail(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      const email = control.value;
+      return control.valueChanges.pipe(
+        tap((_) => {
+          this.isLoading = true;
+        }),
+        debounceTime(500),
+        switchMap((_) => {
+          return this.http.post(
+            [this.authServerPath, 'auth', 'check-email'].join('/'),
+            { email }
+          );
+        }),
+        map((result: any) => {
+          this.isLoading = false;
+          return result ? { hasemail: true } : null;
+        }),
+        take(1)
+      );
+    };
   }
 }
