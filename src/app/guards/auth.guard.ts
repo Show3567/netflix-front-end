47f338962bec75b190e6c83a7d346d82b041084e
show3567 <ydong011@fiu.edu>
2022-05-20 19:54:15 -0500

    update

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,16 +2,10 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
-  CanLoad,
-  Route,
   Router,
   RouterStateSnapshot,
-  UrlSegment,
-  UrlTree,
 } from '@angular/router';
-import { Observable } from 'rxjs';
 import { WithLocalstorageService } from '../services/auth/with-localstorage.service';
-import { UserRole } from '../services/interfaces/user-auth.interface';
 
 @Injectable({
   providedIn: 'root',
