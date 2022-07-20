f514344b25bfc5cd65fe7f1c80136ac48b634ae9
Yue Dong <ydong011@fiu.edu>
2022-07-20 14:54:39 -0500

    update

--- src/app/core/guards/login.guard.ts
+++ src/app/core/guards/login.guard.ts
@@ -4,9 +4,7 @@ import {
   CanActivate,
   Router,
   RouterStateSnapshot,
-  UrlTree,
 } from '@angular/router';
-import { Observable } from 'rxjs';
 import { WithLocalstorageService } from '../../services/auth/with-localstorage.service';
 
 @Injectable({
