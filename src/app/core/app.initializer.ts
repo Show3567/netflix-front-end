c529a6fabeb5fce9f4563d83c5b25e8eff838e51
Yue Dong <ydong011@fiu.edu>
2022-08-05 08:52:32 -0500

    update

--- src/app/core/app.initializer.ts
+++ src/app/core/app.initializer.ts
@@ -1,7 +1,7 @@
 import { WithLocalstorageService } from '../services/auth/with-localstorage.service';
 
 export function appInitializer(authService: WithLocalstorageService) {
-  console.log('this is initialization');
+  console.log('this is Initialization');
   return () =>
     new Promise((resolve) => {
       // attempt to refresh token on app start up to auto authenticate
--- src/app/core/preloading-strategies/movie.preloading.ts
+++ src/app/core/preloading-strategies/movie.preloading.ts
@@ -8,20 +8,17 @@ import { mergeMap } from 'rxjs/operators';
 })
 export class MoviePreloadingStrategy implements PreloadingStrategy {
   preload(route: Route, loadMe: () => Observable<any>): Observable<any> {
-    console.log('route.data: ', route.data);
-
     if (route.data && route.data['preload']) {
       const delay: number = route.data['delay'];
-      console.log('preload called on ' + route.path + ' delay is ' + delay);
+      // console.log('preload called on ' + route.path + ' delay is ' + delay);
 
       return timer(delay).pipe(
         mergeMap((_) => {
-          console.log('Loading now ' + route.path);
+          // console.log('Loading now ' + route.path);
           return loadMe();
         })
       );
     } else {
-      console.log('no preload for the path ' + route.path);
       return of(null);
     }
   }
