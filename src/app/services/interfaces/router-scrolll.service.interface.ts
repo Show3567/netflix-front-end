3178856b9f9b74f2e8b7b2c262489e684d9bf4dd
Yue Dong <ydong011@fiu.edu>
2022-09-21 00:55:29 -0500

    update

---
+++ src/app/services/interfaces/router-scrolll.service.interface.ts
@@ -0,0 +1,52 @@
+import { NavigationEnd, NavigationStart, Route } from '@angular/router';
+
+export interface RouterScrollService {
+  setCustomViewportToScroll(viewport: HTMLElement): void;
+
+  disableScrollDefaultViewport(): void;
+
+  enableScrollDefaultViewPort(): void;
+
+  addStrategyOnceBeforeNavigationForPartialRoute(
+    partialRoute: string,
+    behaviour: RouteScrollBehaviour
+  ): void;
+
+  addStrategyForPartialRoute(
+    partialRoute: string,
+    behaviour: RouteScrollBehaviour
+  ): void;
+
+  removeStrategyForPartialRoute(partialRoute: string): void;
+}
+
+export interface ScrollPositionRestore {
+  event: NavigationStart | NavigationEnd;
+  positions: Record<string, any>;
+  trigger: 'imperative' | 'popstate' | 'hashchange' | undefined;
+  idToRestore: number;
+  routeData?: CustomRouteData;
+}
+
+export interface RouteScrollStrategy {
+  partialRoute: string;
+  behaviour: RouteScrollBehaviour;
+  onceBeforeNavigation?: boolean;
+}
+
+export enum RouteScrollBehaviour {
+  KEEP_POSITION = 'KEEP_POSITION',
+  GO_TO_TOP = 'GO_TO_TOP',
+}
+
+export interface CustomRouteData {
+  scrollBehavior?: RouteScrollBehaviour;
+}
+
+export interface CustomRoute extends Route {
+  data?: CustomRouteData;
+}
+
+export type CustomRoutes = CustomRoute[];
+
+// &https://gist.github.com/dsebastien/1211242afa31e00b2d19fdc3dc6db5e6#file-router-scrolll-service-intf-ts */
