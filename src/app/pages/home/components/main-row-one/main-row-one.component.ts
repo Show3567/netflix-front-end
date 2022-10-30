7f8dc401cdb7c0c10b8f3f372018e9406fadc316
Yue Dong <ydong011@fiu.edu>
2022-10-29 23:51:31 -0500

    update

---
+++ src/app/pages/home/components/main-row-one/main-row-one.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
+
+@Component({
+  selector: 'app-main-row-one',
+  templateUrl: './main-row-one.component.html',
+  styleUrls: ['./main-row-one.component.scss'],
+})
+export class MainRowOneComponent implements OnInit {
+  form!: UntypedFormGroup;
+  get email() {
+    return this.form.get('email');
+  }
+
+  constructor(
+    private fb: UntypedFormBuilder,
+    private readonly router: Router,
+    private readonly authService: AuthService
+  ) {}
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: [
+        '',
+        [Validators.required, Validators.maxLength(45), Validators.email],
+      ],
+    });
+  }
+  ngOnDestroy(): void {}
+
+  onSubmit() {
+    const { jwtToken, tmdb_key } = this.authService.userValue;
+    if (jwtToken) {
+      this.router.navigate(['/movies']);
+    } else {
+      this.router.navigate(['/register/step1']);
+      this.authService.addUserInfo(this.form.value);
+    }
+  }
+}
