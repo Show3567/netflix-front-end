1ca307eafd075996a8e1c43603dfd6e748b4eeb3
Yue Dong <ydong011@fiu.edu>
2022-10-30 00:34:45 -0500

    update

--- src/app/core/pipes/overview-control.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OverviewControlPipe } from './overview-control.pipe';
-
-describe('OverviewControlPipe', () => {
-  it('create an instance', () => {
-    const pipe = new OverviewControlPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
