c1bf593f98426be3df7968b30b676552c5446f83
Yue Dong <ydong011@fiu.edu>
2022-11-02 16:57:50 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,13 @@
-import { APP_INITIALIZER, InjectionToken, NgModule } from '@angular/core';
-import { BrowserModule, Title } from '@angular/platform-browser';
-import {
-  HttpClient,
-  HttpClientModule,
-  HTTP_INTERCEPTORS,
-} from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Router } from '@angular/router';
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 
 import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
-
-import { AuthService } from './services/auth/auth.service';
-import { TmdbService } from './services/tmdb/tmdb.service';
-import { appInitializer } from './core/app.initializer';
-import { AuthWithLocalInterceptor } from './core/interceptors/auth-with-local.interceptor';
-import { ErrorInterceptor } from './core/interceptors/error.interceptor';
-import { WithCookieService } from './services/auth/with-cookie.service';
 
 @NgModule({
   declarations: [AppComponent],
