b200fc7b05bdceb1ec62f313a44dc00ec506868e
Yue Dong <ydong011@fiu.edu>
2022-11-14 11:15:26 -0600

    update

--- src/app/core/app.initializer.ts
+++ src/app/core/app.initializer.ts
@@ -1,12 +1,9 @@
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { AuthNgrxService } from '../Ngrx/Auth/auth-ngrx.service';
-import { AuthService } from '../services/auth/auth.service';
 
 export function appInitializer(authService: AuthNgrxService) {
   console.log('this is Initialization');
-  return () =>
-    new Promise((resolve) => {
-      // attempt to refresh token on app start up to auto authenticate
-      authService.refreshToken();
-      // .subscribe().add(resolve);
-    });
+
+  return () => of().pipe(tap(() => authService.refreshToken()));
 }
