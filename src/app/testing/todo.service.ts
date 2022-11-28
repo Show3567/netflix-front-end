28729f1ca875a1a1fd83957835ba2edcf0105147
Yue Dong <ydong011@fiu.edu>
2022-11-28 00:12:34 -0600

    update

--- src/app/testing/todo.service.ts
+++ src/app/testing/todo.service.ts
@@ -1,9 +1,18 @@
+import { HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  constructor(private fetch: Function = window.fetch.bind(window)) {}
 
-  constructor() { }
+  async getTodos(): Promise<string[]> {
+    const response = await this.fetch('/todo');
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status} ${response.statusText}`);
+    }
+
+    return await response.json();
+  }
 }
