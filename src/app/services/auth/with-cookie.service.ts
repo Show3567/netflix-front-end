13a2ffde470c04e6fe33a06733ed12a496091090
Yue Dong <ydong011@fiu.edu>
2022-12-24 20:14:38 +0800

    update

--- src/app/services/auth/with-cookie.service.ts
+++ src/app/services/auth/with-cookie.service.ts
@@ -7,12 +7,13 @@ import { catchError, map, tap } from 'rxjs/operators';
 import { AUTHSERVER } from 'src/app/core/core.module';
 import { AppUserAuthCookie } from '../interfaces/user-auth-c.interface';
 import { AppUserAuth, UserRole } from '../interfaces/user-auth.interface';
+import { AppUserRegister, UserInfo } from '../interfaces/user-signup.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WithCookieService {
-  private refreshTokenTimeout!: ReturnType<typeof setTimeout>;
+  private appUserRegister = new AppUserRegister();
 
   private userSubject$!: BehaviorSubject<AppUserAuth>;
   user$!: Observable<AppUserAuth>;
@@ -30,40 +31,40 @@ export class WithCookieService {
     this.user$ = this.userSubject$.asObservable();
   }
 
-  // /* SignUp */
-  // addUserInfo(userInfo: UserInfo) {
-  //   this.appUserRegister = {
-  //     ...this.appUserRegister,
-  //     ...userInfo,
-  //   };
-  // }
-  // sighup(userRole: { role: UserRole }): Observable<AuthDto | string> {
-  //   this.appUserRegister = {
-  //     ...this.appUserRegister,
-  //     ...userRole,
-  //   };
-  //   const { username, password, email, role, tmdb_key } = this.appUserRegister;
+  /* SignUp */
+  addUserInfo(userInfo: UserInfo) {
+    this.appUserRegister = {
+      ...this.appUserRegister,
+      ...userInfo,
+    };
+  }
+  sighup(userRole: { role: UserRole }): Observable<AppUserAuthCookie | string> {
+    this.appUserRegister = {
+      ...this.appUserRegister,
+      ...userRole,
+    };
+    const { username, password, email, role, tmdb_key } = this.appUserRegister;
 
-  //   if (!username || !password || !email || !role || !tmdb_key)
-  //     return of('Register failed');
+    if (!username || !password || !email || !role || !tmdb_key)
+      return of('Register failed');
 
-  //   return this.http
-  //     .post<AuthDto>(
-  //       [this.authServerPath, 'auth', 'signup'].join('/'),
-  //       this.appUserRegister
-  //     )
-  //     .pipe(
-  //       tap(({ accessToken, role }: AuthDto) => {
-  //         this.setUserValueByToken({ accessToken, role });
-  //         this.router.navigate(['/movies']);
-  //       }),
-  //       catchError((error) => {
-  //         return throwError('SomeThing Wrong during sign up!', error);
-  //       })
-  //     );
-  // }
+    return this.http
+      .post<AppUserAuthCookie>(
+        `${this.authServerPath}/auth-c/signup`,
+        this.appUserRegister
+      )
+      .pipe(
+        tap((user: AppUserAuthCookie) => {
+          this.userSubject$.next(user);
+          this.router.navigate(['/movies']);
+        }),
+        catchError((error) => {
+          return throwError('SomeThing Wrong during sign up!', error);
+        })
+      );
+  }
 
-  login(username: string, password: string) {
+  login(username: string, password: string): Observable<AppUserAuthCookie> {
     return this.http
       .post<AppUserAuthCookie>(
         `${this.authServerPath}/auth-c/signin`,
@@ -73,7 +74,6 @@ export class WithCookieService {
       .pipe(
         map((user: AppUserAuthCookie) => {
           this.userSubject$.next(user);
-          this.startRefreshTokenTimer();
           return user;
         })
       );
@@ -82,40 +82,7 @@ export class WithCookieService {
   logout() {
     this.http.get(`${this.authServerPath}/auth-c/signout`).subscribe();
 
-    this.stopRefreshTokenTimer();
     this.userSubject$.next({});
     this.router.navigate(['/home']);
-  }
-
-  refreshToken() {
-    return this.http
-      .post<any>(
-        `${this.authServerPath}/auth-c/refresh-token`,
-        {},
-        { withCredentials: true }
-      )
-      .pipe(
-        map((user) => {
-          this.userSubject$.next(user);
-          this.startRefreshTokenTimer();
-          return user;
-        })
-      );
-  }
-
-  //* helper methods;
-  private startRefreshTokenTimer() {
-    if (this.userValue) {
-      // & maybe chech cookie expire time !!!!
-      this.refreshTokenTimeout = setTimeout(
-        () => this.refreshToken().subscribe(),
-        0
-        // timeout
-      );
-    }
-  }
-
-  private stopRefreshTokenTimer() {
-    clearTimeout(this.refreshTokenTimeout);
   }
 }
