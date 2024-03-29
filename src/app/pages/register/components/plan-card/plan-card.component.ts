20d00c8334c6acd5113b592d599e39f7868369d8
Yue Dong <ydong011@fiu.edu>
2024-03-29 14:37:46 -0500

    update

--- src/app/pages/register/components/plan-card/plan-card.component.ts
+++ src/app/pages/register/components/plan-card/plan-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Plan } from 'src/app/services/interfaces/plan.interface';
 
 @Component({
@@ -10,43 +10,56 @@ export class PlanCardComponent {
   selectedPlan: 'Standard with ads' | 'Standard' | 'Premium' =
     'Standard with ads';
 
-  plans: Plan[] = [
-    {
-      id: 1,
-      planTitle: 'Standard with ads',
-      resolutionTitle: '1080p',
-      monthlyPrice: 6.99,
-      vsQuality: 'Good',
-      resolution: '1080p (Full HD)',
-      supportedDevices: ['TV', 'computer', 'mobile phone', 'tablet'],
-      shareNum: 2,
-      downloadDevices: 2,
-      ads: 'A few ad breaks',
-    },
-    {
-      id: 2,
-      planTitle: 'Standard',
-      resolutionTitle: '1080p',
-      monthlyPrice: 15.49,
-      vsQuality: 'Good',
-      resolution: '1080p (Full HD)',
-      supportedDevices: ['TV', 'computer', 'mobile phone', 'tablet'],
-      shareNum: 2,
-      downloadDevices: 2,
-      ads: 'No ads',
-    },
-    {
-      id: 3,
-      planTitle: 'Premium',
-      resolutionTitle: '4K + HDR',
-      monthlyPrice: 22.99,
-      vsQuality: 'Best',
-      resolution: '4K (Ultra HD) + HR',
-      spatialAudio: 'Incuded',
-      supportedDevices: ['TV', 'computer', 'mobile phone', 'tablet'],
-      shareNum: 4,
-      downloadDevices: 6,
-      ads: 'A few ad breaks',
-    },
-  ];
+  @Input() plan: Plan = {
+    id: 1,
+    planTitle: 'Standard with ads',
+    resolutionTitle: '1080p',
+    monthlyPrice: 6.99,
+    vsQuality: 'Good',
+    resolution: '1080p (Full HD)',
+    supportedDevices: ['TV', 'computer', 'mobile phone', 'tablet'],
+    shareNum: 2,
+    downloadDevices: 2,
+    ads: 'A few ad breaks',
+  };
+
+  // plans: Plan[] = [
+  //   {
+  //     id: 1,
+  //     planTitle: 'Standard with ads',
+  //     resolutionTitle: '1080p',
+  //     monthlyPrice: 6.99,
+  //     vsQuality: 'Good',
+  //     resolution: '1080p (Full HD)',
+  //     supportedDevices: ['TV', 'computer', 'mobile phone', 'tablet'],
+  //     shareNum: 2,
+  //     downloadDevices: 2,
+  //     ads: 'A few ad breaks',
+  //   },
+  //   {
+  //     id: 2,
+  //     planTitle: 'Standard',
+  //     resolutionTitle: '1080p',
+  //     monthlyPrice: 15.49,
+  //     vsQuality: 'Good',
+  //     resolution: '1080p (Full HD)',
+  //     supportedDevices: ['TV', 'computer', 'mobile phone', 'tablet'],
+  //     shareNum: 2,
+  //     downloadDevices: 2,
+  //     ads: 'No ads',
+  //   },
+  //   {
+  //     id: 3,
+  //     planTitle: 'Premium',
+  //     resolutionTitle: '4K + HDR',
+  //     monthlyPrice: 22.99,
+  //     vsQuality: 'Best',
+  //     resolution: '4K (Ultra HD) + HR',
+  //     spatialAudio: 'Incuded',
+  //     supportedDevices: ['TV', 'computer', 'mobile phone', 'tablet'],
+  //     shareNum: 4,
+  //     downloadDevices: 6,
+  //     ads: 'A few ad breaks',
+  //   },
+  // ];
 }
